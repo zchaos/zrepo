31641071927912fdcbd45fcd988c77c47a23fe49
zhuchx <zhuchx1008@gmail.com>
2013-12-04 11:32:27 +0800

    转换为插件工程，并实现依赖排序

---
+++ zplugins/src/main/java/com/zchaos/zplugins/components/dependencies/sort/DependenciesSort.java
@@ -0,0 +1,27 @@
+package com.zchaos.zplugins.components.dependencies.sort;
+
+import org.eclipse.jdt.internal.ui.packageview.PackageExplorerPart;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.ViewerComparator;
+
+public class DependenciesSort {
+
+	public void sort() {
+		PackageExplorerPart part = getPart();
+		if (part == null) {
+			return;
+		}
+		TreeViewer view = part.getTreeViewer();
+
+		ViewerComparator comparator = view.getComparator();
+
+		if (!(comparator instanceof JavaElementComparator_Dependencies)) {
+			JavaElementComparator_Dependencies dc = new JavaElementComparator_Dependencies();
+			view.setComparator(dc);
+		}
+	}
+
+	private PackageExplorerPart getPart() {
+		return PackageExplorerPart.getFromActivePerspective();
+	}
+}
